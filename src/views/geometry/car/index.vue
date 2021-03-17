ee1480a4daa2a02b7853bd638c36044538373b1e
baix@shumei.ai <baix@shumei.ai>
2021-03-17 17:59:19 +0800

    crossbeam

--- src/views/geometry/car/index.vue
+++ src/views/geometry/car/index.vue
@@ -13,9 +13,10 @@ export default defineComponent({
   setup () {
     let scene, camera, renderer
     let ambientLight, light
-    let group
     let controls
+    let group
     const color = 0x2C85E1
+    const material = new THREE.MeshPhongMaterial({ color })
     const extrudeSettings = {
       depth: 50,
       bevelEnabled: true,
@@ -52,49 +53,17 @@ export default defineComponent({
       scene.add(ambientLight)
       scene.add(light)
 
-      // material
-      const material = new THREE.MeshPhongMaterial({ color })
-
       // group
       group = new THREE.Group()
       scene.add(group)
 
-      // 底盘
-      const cylinder = new THREE.CylinderGeometry(100, 100, 20, 20)
-      const cylinderMesh = new THREE.Mesh(cylinder, material)
-      group.add(cylinderMesh)
+      // wheel
+      addWheel(0, 0, 400, Math.PI * 0.5, 0, 0)
+      addWheel(300, 0, 400, Math.PI * 0.5, 0, 0)
+      addWheel(0, 0, 0, -Math.PI * 0.5, 0, 0)
+      addWheel(300, 0, 0, -Math.PI * 0.5, 0, 0)
 
-      // 中心轴
-      const cylinder1 = new THREE.CylinderGeometry(40, 40, 40, 30)
-      const cylinderMesh1 = new THREE.Mesh(cylinder1, material)
-      cylinderMesh1.position.set(0, 30, 0)
-      group.add(cylinderMesh1)
-
-      // 中心轴2
-      const arcShape1 = new THREE.Shape()
-        .absarc(0, 0, 35, 0, Math.PI * 2, false)
-
-      // 上圆环 shape + ExtrudeGeometry 挤压
-      const arcShape = new THREE.Shape()
-        .absarc(0, 0, 85, 0, Math.PI * 2, false)
-      const holePath = new THREE.Path()
-        .absarc(0, 0, 80, 0, Math.PI * 2, true)
-      arcShape.holes.push(holePath)
-
-      // 叶子板
-      const triangleShape = new THREE.Shape()
-        .moveTo(0, 0)
-        .lineTo(0, 40)
-        .lineTo(30, 15)
-        .lineTo(40, 0)
-        .lineTo(0, 0)
-
-      addShape(arcShape, extrudeSettings, 0, 60, 0, Math.PI * 0.5, 0, 0)
-      addShape(arcShape1, extrudeSettings1, 0, 60, 0, Math.PI * 0.5, 0, 0)
-      addShape(triangleShape, extrudeSettings2, 40, 10, 0, 0, 0, 0)
-      addShape(triangleShape, extrudeSettings2, 0, 10, -40, 0, Math.PI * 0.5, 0)
-      addShape(triangleShape, extrudeSettings2, 0, 10, 40, 0, Math.PI * 1.5, 0)
-      addShape(triangleShape, extrudeSettings2, -40, 10, 0, 0, Math.PI, 0)
+      crossbeam(0, 0, 160, 0, 0, 0)
 
       // renderer
       renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -115,7 +84,69 @@ export default defineComponent({
       const mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({ color }))
       mesh.position.set(x, y, z)
       mesh.rotation.set(rx, ry, rz)
+      return mesh
+    }
+
+    function addWheel (x, y, z, rx, ry, rz) {
+      // 底盘
+      const mesh = new THREE.Mesh(
+        new THREE.CylinderGeometry(100, 100, 20, 20),
+        material
+      )
       group.add(mesh)
+
+      // 中心轴
+      const cylinder1 = new THREE.CylinderGeometry(40, 40, 40, 30)
+      const cylinderMesh1 = new THREE.Mesh(cylinder1, material)
+      cylinderMesh1.position.set(0, 30, 0)
+      mesh.add(cylinderMesh1)
+
+      // 中心轴2
+      const arcShape1 = new THREE.Shape()
+        .absarc(0, 0, 35, 0, Math.PI * 2, false)
+
+      // 上圆环 shape + ExtrudeGeometry 挤压
+      const arcShape = new THREE.Shape()
+        .absarc(0, 0, 85, 0, Math.PI * 2, false)
+      const holePath = new THREE.Path()
+        .absarc(0, 0, 80, 0, Math.PI * 2, true)
+      arcShape.holes.push(holePath)
+
+      // 叶子板
+      const triangleShape = new THREE.Shape()
+        .moveTo(0, 0)
+        .lineTo(0, 40)
+        .lineTo(30, 15)
+        .lineTo(40, 0)
+        .lineTo(0, 0)
+
+      mesh.add(addShape(arcShape, extrudeSettings, 0, 60, 0, Math.PI * 0.5, 0, 0))
+      mesh.add(addShape(arcShape1, extrudeSettings1, 0, 60, 0, Math.PI * 0.5, 0, 0))
+      mesh.add(addShape(triangleShape, extrudeSettings2, 40, 10, 0, 0, 0, 0))
+      mesh.add(addShape(triangleShape, extrudeSettings2, 0, 10, -40, 0, Math.PI * 0.5, 0))
+      mesh.add(addShape(triangleShape, extrudeSettings2, 0, 10, 40, 0, Math.PI * 1.5, 0))
+      mesh.add(addShape(triangleShape, extrudeSettings2, -40, 10, 0, 0, Math.PI, 0))
+
+      mesh.position.set(x, y, z)
+      mesh.rotation.set(rx, ry, rz)
+    }
+
+    function crossbeam (x, y, z, rx, ry, rz) {
+      const mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(50, 40, 460),
+        material
+      )
+
+      const mesh1 = new THREE.Mesh(
+        new THREE.BoxGeometry(50, 40, 460),
+        material
+      )
+      mesh1.position.set(300, 0, 0)
+      mesh.add(mesh1)
+
+      group.add(mesh)
+      mesh.position.set(x, y, z)
+      mesh.rotation.set(rx, ry, rz)
     }
 
     function onWindowResize () {
---
+++ src/views/geometry/teapot.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="GeoTeapotWrapper"></div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry'
+
+import { defineComponent, onMounted } from 'vue'
+export default defineComponent({
+  name: 'GeoTeapot',
+  setup () {
+    let scene, camera, renderer
+    let cameraControls
+
+    let ambientLight, light
+
+    let teapot
+    let phongMaterial // 光照材质
+
+    const controls = {
+      teapotSize: 200,
+      tess: -1,
+      bottom: true,
+      lid: true,
+      body: true,
+      fitLid: false,
+      nonblinn: true
+    }
+
+    function init () {
+      // camera
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 80000)
+      camera.position.set(-600, 500, 1300)
+
+      // light
+      ambientLight = new THREE.AmbientLight(0x333333)
+      light = new THREE.DirectionalLight(0xFFFFFF, 1.0)
+
+      // material
+      const materialColor = new THREE.Color()
+      materialColor.setRGB(1.0, 1.0, 1.0)
+      phongMaterial = new THREE.MeshPhongMaterial({
+        color: materialColor,
+        side: THREE.DoubleSide
+      })
+
+      scene = new THREE.Scene()
+      scene.background = new THREE.Color(0xAAAAAA)
+
+      scene.add(ambientLight)
+      scene.add(light)
+
+      renderer = new THREE.WebGLRenderer({ antialias: true })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      const container = document.getElementById('GeoTeapotWrapper')
+      container.appendChild(renderer.domElement)
+
+      cameraControls = new OrbitControls(camera, renderer.domElement)
+      cameraControls.addEventListener('change', render)
+
+      window.addEventListener('resize', onWindowResize)
+    }
+
+    function render () {
+      createNewTeapot()
+      renderer.render(scene, camera)
+    }
+
+    function createNewTeapot () {
+      if (teapot !== undefined) {
+        teapot.geometry.dispose()
+        scene.remove(teapot)
+      }
+
+      const teapotGeometry = new TeapotGeometry(
+        controls.teapotSize,
+        controls.tess,
+        controls.bottom,
+        controls.lid,
+        controls.body,
+        controls.fitLid,
+        controls.nonblinn
+      )
+
+      teapot = new THREE.Mesh(teapotGeometry, phongMaterial)
+      scene.add(teapot)
+    }
+
+    function onWindowResize () {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      render()
+    }
+
+    onMounted(() => {
+      init()
+      render()
+    })
+    return {}
+  }
+})
+</script>
+
+<style lang='less' scoped>
+</style>
